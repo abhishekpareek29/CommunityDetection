85a724007c16c473418a89226243ed2a5788764d
Abhishek Pareek <abhishekpareek29@gmail.com>
2019-03-10 12:13:01 -0400

    update final

--- sac1.py
+++ sac1.py
@@ -1,179 +1,190 @@
-import csv
+import pandas as pd
+import numpy as np
+import sys
 from igraph import *
 from scipy import spatial
 
-g = Graph()
-attributes = []
+simMatrix = []
+simMatrix2 = []
+
+# Implements phase1.
+def phase1(g, alpha, C):
+	V = len(g.vs)
+	m = len(g.es)
+	iteration = 0
+	check = 0
+
+	while (check == 0 and iteration < 15):
+		check = 1
+		for vi in range(V):
+			maxV = -1
+			maxDeltaQ = 0.0
+			clusters = list(set(C))
+			for vj in clusters:
+				if (C[vi] != vj):
+					dQ = (alpha*change_mod(C, g, vi, vj)) + ((1-alpha)*change_attr_sim(C, g, vi, vj))
+					if (dQ > maxDeltaQ):
+						maxDeltaQ = dQ
+						maxV = vj
+			if (maxDeltaQ > 0.0 and maxV != -1):
+				check = 0
+				C[vi] = maxV
+		iteration = iteration + 1
+	return C
+
+# Implements phase2.
+def phase2(g, C):
+	newC = sequential_clusters(C)
+	temp = list(Clustering(newC))
+	L = len(set(newC))
+	simMatrix = np.zeros((L,L))
+
+	for i in range(L):
+		for j in range(L):
+			similarity = 0.0
+			for k in temp[i]:
+				for l in temp[j]:
+					similarity = similarity + simMatrix2[k][l]
+			simMatrix[i][j] = similarity
+
+	g.contract_vertices(newC)
+	g.simplify(combine_edges=sum)
+	return
+
+# Make the clusters sequential.
+def sequential_clusters(C):
+	mapping = {}
+	newC = []
+	c = 0
+	for i in C:
+		if i in mapping:
+			newC.append(mapping[i])
+		else:
+			newC.append(c)
+			mapping[i] = c
+			c = c + 1
+	return newC
+
+# Change in modularity.
+def change_mod(C, g, v1, v2):
+	Q1 = g.modularity(C, weights='weight')
+	temp = C[v1]
+	C[v1] = v2
+	Q2 = g.modularity(C, weights='weight')
+	C[v1] = temp
+	return (Q2-Q1);
+
+# Change in attribute similarity.
+def change_attr_sim(C, g, v1, v2):
+	S = 0.0;
+	indices = [i for i, x in enumerate(C) if x == v2]
+	for v in indices:
+		S = S + simMatrix[v1][v]
+	return S/(len(indices)*len(set(C)))
+
+# Total attribute similarity.
+def total_attr_sim(C, g):
+	clusters = list(Clustering(C))
+	V = g.vcount()
+	S = 0.0
+	for c in clusters:
+		T = 0.0
+		for v1 in c:
+			for v2 in C:
+				if (v1 != v2):
+					T = T + simMatrix[v1][v2]
+		T = T/len(c)
+		S = S + T
+	return S/(len(set(C)))
+
+def main(alpha):
+	# Create igraph.
+	attributes = pd.read_csv('data/fb_caltech_small_attrlist.csv')
+
+	V = len(attributes)
+
+	with open('data/fb_caltech_small_edgelist.txt') as f:
+		edges = f.readlines()
+	edges = [tuple([int(x) for x in line.strip().split(" ")]) for line in edges]
+
+	g = Graph()
+	g.add_vertices(V)
+	g.add_edges(edges)
+	g.es['weight'] = [1]*len(edges)
+
+	for col in attributes.keys():
+		g.vs[col] = attributes[col]
+
+	# Pre-Computing Similarity Matrix
+	global simMatrix
+	global simMatrix2
+	simMatrix = np.zeros((V,V))
+	for i in range(V):
+		for j in range(V):
+			simMatrix[i][j] = 1 - spatial.distance.cosine(list(g.vs[i].attributes().values()), list(g.vs[j].attributes().values()))
+
+	# Copy.
+	simMatrix2 = np.array(simMatrix)
+
+	# Run Algorithm
+	V = g.vcount()
+	C = phase1(g, alpha, list(range(V)))
+	print('Number of Communities after Phase 1')
+	print(len(set(C)))
+	C = sequential_clusters(C)
+
+	# Modularity of phase 1 clustering
+	mod1 = g.modularity(C, weights='weight') + total_attr_sim(C, g)
+
+	# Phase 2
+	phase2(g, C)
+
+	# Run phase 1 again.
+	V = g.vcount()
+	C2 = phase1(g, alpha, list(range(V)))
+	C2new = sequential_clusters(C2)
+	clustersPhase2 = list(Clustering(C2new))
+	#Composite modularity of contracted clustering
+	mod2 = g.modularity(C, weights='weight') + total_attr_sim(C, g)
+
+	finalC = []
+	C1new = sequential_clusters(C)
+	clustersPhase1 = list(Clustering(C1new))
+
+	# Mapping the super clusters from phase 2 to original vertices.
+	for c in clustersPhase2:
+		t = []
+		for v in c:
+			t.extend(clustersPhase1[v])
+		finalC.append(t)
 
 
-def readUsers():
-    with open('data/fb_caltech_small_attrlist.csv') as f:
-        csvReader = csv.reader(f, delimiter=',')
-        header = next(csvReader)
-        for attr in header:
-            attributes.append(attr)
+	# Write clusters to output file.
+	if alpha == 0.5:
+		alpha = str(5)
+	elif alpha == 0.0:
+		alpha = str(0)
+	elif alpha == 1.0:
+		alpha = str(1)
 
-        userId = 0
-        for user in csvReader:
-            g.add_vertex(userId)
-            for attributeID in range(0, len(attributes)):
-                g.vs[userId][attributes[attributeID]] = float(user[attributeID])
-            userId += 1
+	if (mod1 > mod2):
+		clusters = clustersPhase1
+	else:
+		clusters = clustersPhase2
 
+	file = open("communities_" + alpha + ".txt", 'w+')
+	for c in clusters:
+		for i in range(len(c)-1):
+			file.write("%s," % c[i])
+		file.write(str(c[-1]))
+		file.write('\n')
+	file.close()
 
-def readEdges():
-    with open('data/fb_caltech_small_edgelist.txt') as f:
-        lines = f.readlines()
-        for edge in lines:
-            edge = edge.strip().split(' ')
-            source = int(edge[0])
-            destination = int(edge[1])
-            g.add_edge(source, destination)
-            g.es[g.get_eid(source, destination)]["weight"] = 1
+	return clusters
 
 
-def sumOfEdgeWeightsForVertex(vertex):
-    sumOfEdges = 0
-    currentEdges = g.incident(vertex)
-    for edge in currentEdges:
-        sumOfEdges += g.es[edge]["weight"]
-    return sumOfEdges
-
-
-def calculateQNewman(setOfVertices, currentVertex):
-    communityDegree = 0
-    links = 0
-    numOfEdges = g.ecount()
-    for vertex in setOfVertices:
-        if g.are_connected(currentVertex, vertex):
-            links += g.es[g.get_eid(currentVertex, vertex)]["weight"]
-    currentVertexDegree = sumOfEdgeWeightsForVertex(currentVertex)
-    for vertex in setOfVertices:
-        communityDegree += sumOfEdgeWeightsForVertex(vertex)
-    QNewman = (1.0 / (2 * numOfEdges)) * (links - ((float(currentVertexDegree) / (2 * numOfEdges)) * communityDegree))
-    return QNewman
-
-
-def sac1(alpha):
-    numOfVertices = g.vcount()
-    numOfEdges = g.ecount()
-    communities = dict()
-    for communityIndex in range(0, numOfVertices):
-        communities[communityIndex] = [communityIndex]
-    cosineSimilarityMatrix = [[0 for x in range(numOfVertices)] for y in range(numOfVertices)]
-    for idA in range(numOfVertices):
-        for idB in range(idA, numOfVertices):
-            attrA = []
-            attrB = []
-            for attribute in attributes:
-                attrA.append(g.vs[idA][attribute])
-                attrB.append(g.vs[idB][attribute])
-            cosineSimilarityMatrix[idA][idB] = 1 - spatial.distance.cosine(map(float, attrA), map(float, attrB))
-            cosineSimilarityMatrix[idB][idA] = cosineSimilarityMatrix[idA][idB]
-    if numOfEdges > 0:
-        vertexList = list(range(0, numOfVertices))
-        for phaseOneIteration in range(0, 15):
-            print('\t Phase One iteration: {}'.format(phaseOneIteration + 1))
-            convergence = True
-            for currentVertex in vertexList:
-                communityWithMaxGain = -1
-                maxCompositeModularityGain = 0
-                originalCommunity = -1
-                for community in communities.keys():
-                    if currentVertex in communities[community]:
-                        originalCommunity = community
-                        communities[originalCommunity].remove(currentVertex)
-                        break
-                previousCosineSimilarity = []
-                for x in communities[originalCommunity]:
-                    previousCosineSimilarity.append(cosineSimilarityMatrix[x][currentVertex])
-                previousCosineSimilarity = mean(previousCosineSimilarity)
-                communities[originalCommunity].append(currentVertex)
-                for community in communities.keys():
-                    if community == originalCommunity:
-                        continue
-                    cosine = []
-                    for x in communities[community]:
-                        cosine.append(cosineSimilarityMatrix[x][currentVertex])
-                    QNewman = calculateQNewman(communities[community], currentVertex)
-                    QAttribute = mean(cosine)
-                    Q = alpha * QNewman + (1 - alpha) *  (QAttribute - previousCosineSimilarity)
-                    if Q > 0 and Q > maxCompositeModularityGain:
-                        maxCompositeModularityGain = Q
-                        communityWithMaxGain = community
-                if communityWithMaxGain != -1:
-                    convergence = False
-                    communities[communityWithMaxGain].append(currentVertex)
-                    communities[originalCommunity].remove(currentVertex)
-                    if not len(communities[originalCommunity]):
-                        del communities[originalCommunity]
-            print('\t\t', len(communities))
-            if convergence:
-                break
-    return communities
-
-
-def communityToNode(communities):
-    communityIndex = 0
-    membership = list(range(0, g.vcount()))
-    for community in communities.keys():
-        for vertex in communities[community]:
-            membership[vertex] = communityIndex
-        communityIndex += 1
-    g.contract_vertices(membership, combine_attrs=mean)
-    g.simplify(combine_edges=sum)
-
-
-def mergeCommunities(previousCommunities, communities, iteration):
-    if iteration == 0:
-        communityIndex = 0
-        for community in communities.keys():
-            previousCommunities[communityIndex] = communities[community]
-            communityIndex += 1
-    else:
-        newCommunities = dict()
-        communityIndex = 0
-        for community in communities.keys():
-            temp = []
-            for vertex in communities[community]:
-                temp.extend(previousCommunities[vertex])
-            newCommunities[communityIndex] = temp
-            communityIndex += 1
-        previousCommunities = newCommunities.copy()
-    return previousCommunities
-
-
-def writeCommunities(communities, alpha):
-    if alpha == 0.5:
-        alpha = 5
-    fileName = "communities_" + str(int(alpha)) + ".txt"
-    with open(fileName, 'w') as f:
-        for community in communities.keys():
-            for vertex in sorted(communities[community]):
-                f.write(str(vertex) + ", ")
-            f.write("\n")
-
-
-def main():
-    if len(sys.argv) <= 1:
-        print("python sac1.py <alpha> \n Alpha can be 0, 0.5, or 1.")
-        exit()
-    alpha = float(sys.argv[1])
-    readUsers()
-    readEdges()
-    communities = dict()
-    previousCommunities = dict()
-    for iteration in range(0, 15):
-        print("Main iteration: {}".format(iteration + 1))
-        if iteration != 0:
-            communityToNode(communities)
-        communities = sac1(alpha)
-        if len(communities) == len(previousCommunities):
-            break
-        else:
-            previousCommunities = mergeCommunities(previousCommunities, communities, iteration)
-    writeCommunities(previousCommunities, alpha)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	if len(sys.argv) <= 1:
+		print("Missing argument.")
+	else:
+		main(float(sys.argv[1]))
